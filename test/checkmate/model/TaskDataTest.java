a0c6f7db343975fdcedfa851ec3a7b93ff5d2681
jitkone <juha.itkonen@iki.fi>
2016-09-05 21:13:36 +0300

    Refactoring test code location and building IDEA project

---
+++ test/checkmate/model/TaskDataTest.java
@@ -0,0 +1,150 @@
+package checkmate.model;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by jitkonen on 05.09.2016.
+ */
+public class TaskDataTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testIncrementTime() throws Exception {
+
+    }
+
+    public void testRollbackSession() throws Exception {
+
+    }
+
+    public void testGetTimeToday() throws Exception {
+
+    }
+
+    public void testGetWeekTime() throws Exception {
+
+    }
+
+    public void testGetMonthTime() throws Exception {
+
+    }
+
+    public void testGetYearTime() throws Exception {
+
+    }
+
+    public void testGetTimePerDay() throws Exception {
+
+    }
+
+    public void testGetNDaysTime() throws Exception {
+
+    }
+
+    public void testDecreaseTimePerDay() throws Exception {
+
+    }
+
+    public void testResetSessionTime() throws Exception {
+
+    }
+
+    public void testGetTime() throws Exception {
+
+    }
+
+    public void testGetSessionTime() throws Exception {
+
+    }
+
+    public void testGetTaskName() throws Exception {
+
+    }
+
+    public void testGetCategoryName() throws Exception {
+
+    }
+
+    public void testGetTaskType() throws Exception {
+
+    }
+
+    public void testGetTaskID() throws Exception {
+
+    }
+
+    public void testGenerateTaskID() throws Exception {
+
+    }
+
+    public void testSetTaskName() throws Exception {
+
+    }
+
+    public void testSetCategoryName() throws Exception {
+
+    }
+
+    public void testSetSortKey() throws Exception {
+
+    }
+
+    public void testGetSortKey() throws Exception {
+
+    }
+
+    public void testSetCategoryName1() throws Exception {
+
+    }
+
+    public void testGetTargetSpendingLevel() throws Exception {
+
+    }
+
+    public void testGetTargetSpendingLevelType() throws Exception {
+
+    }
+
+    public void testGetTargetSpendingLevelString() throws Exception {
+
+    }
+
+    public void testSetTargetSpendingLevel() throws Exception {
+
+    }
+
+    public void testIsHidden() throws Exception {
+
+    }
+
+    public void testSetHidden() throws Exception {
+
+    }
+
+    public void testIsDead() throws Exception {
+
+    }
+
+    public void testSetDead() throws Exception {
+
+    }
+
+    public void testGetLastEntryDate() throws Exception {
+
+    }
+
+    public void testSetLastEntryDate() throws Exception {
+
+    }
+
+    public void testAddLastEntryDate() throws Exception {
+
+    }
+
+}
